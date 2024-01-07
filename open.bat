569fcfbc6ff77848041fc0d66dc2f1e575187648
Hamidul Islam <hamidthedev@gmail.com>
2024-01-08 00:22:35 +0600

    code updated

--- open.bat
+++ open.bat
@@ -7,7 +7,7 @@ set DirectoryPath="D:\Development\PYTHON\DSA Python - Coding Ninja"
 rem Change to the directory
 cd /d %DirectoryPath%
 
-rem Open Visual Studio Code in the current directory and start it in a new window
-start code .
+rem Open Visual Studio Code in the current directory and start it without keeping the command prompt window open
+start /B code .
 
 endlocal
