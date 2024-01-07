886f7324cf2e33558c063c27b4fadc0751f491fd
Hamidul Islam <hamidthedev@gmail.com>
2024-01-08 00:12:04 +0600

    code updated

--- open.bat
+++ open.bat
@@ -5,9 +5,9 @@ rem Specify the path to the directory you want to open
 set DirectoryPath="D:\Development\PYTHON\DSA Python - Coding Ninja"
 
 rem Change to the directory
-cd %DirectoryPath%
+cd /d %DirectoryPath%
 
-rem Open Visual Studio Code in the current directory
-code .
+rem Open Visual Studio Code in the current directory and start it in a new window
+start code .
 
 endlocal
