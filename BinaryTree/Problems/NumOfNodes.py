3fbc7fcf8838912ad6f50192dc2232682ef8474a
Hamidul Islam <hamidthedev@gmail.com>
2024-01-08 21:26:21 +0600

    code updated

--- BinaryTree/Problems/NumOfNodes.py
+++ BinaryTree/Problems/NumOfNodes.py
@@ -16,7 +16,7 @@ rootNode.right = node3
 def numOfNodes(root):
     if root is None:
         return 0
-    leftNodeCount = numOfNodes(root.left) #numOfNodes() function will keep calling for left node untill it's reached to the None. then it will start calling root.right and if right has left node, it will again start calling 
+    leftNodeCount = numOfNodes(root.left) #numOfNodes() function will keep calling for left node untill it's reached to the None. then it will start calling root.right and if right has left node, it will again start calling left
     rightNodeCount = numOfNodes(root.right)
     
     return leftNodeCount + rightNodeCount + 1 #adding +1 for every return
