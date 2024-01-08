179d3153dff8763b59eaae3ceb133d7f2f2e8162
Hamidul Islam <hamidthedev@gmail.com>
2024-01-08 13:05:33 +0600

    code updated

--- BinaryTree/BinaryTree.py
+++ BinaryTree/BinaryTree.py
@@ -54,7 +54,7 @@ def treeInput():
   if(data == -1):
     return None #-1 means the node would be None and parent node will be linked to None as their left or right node
   
-  #creating nodes from input
+  #creating nodes f
   rootNode = TreeNode(data)
   leftNode = TreeNode(treeInput())
   rightNode = TreeNode(treeInput())
