6d900bd749eaf4a55c814a593f86efa8f4287d52
Hamidul Islam <hamidthedev@gmail.com>
2024-01-08 13:56:35 +0600

    code updated

--- BinaryTree/BinaryTree.py
+++ BinaryTree/BinaryTree.py
@@ -37,4 +37,4 @@ def treeInput():
   return rootNode #it will return the firs-most root node of the whole tree which is needed for printing nodes
 
 #taking input and printing
-printTreeDetailed(treeInput())
+printTree(treeInput())
