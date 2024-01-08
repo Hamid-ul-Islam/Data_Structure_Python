b763c93522a1258cadc1742b70fc1ad041bbf676
Hamidul Islam <hamidthedev@gmail.com>
2024-01-08 22:03:36 +0600

    code updated

--- BinaryTree/Problems/HeightOfTree.py
+++ BinaryTree/Problems/HeightOfTree.py
@@ -13,7 +13,7 @@ node3 = TreeNode(3)
 rootNode.left = node2
 rootNode.right = node3
 
-#logic: lastNode will return 0 and 0 from left and right as they are None. then we find max of them. and we 
+#logic: lastNode will return 0 and 0 from left and right as they are None. then we find max of them. and we add +1 
 def heightOfTree(root):
     if root is None:
         return 0
