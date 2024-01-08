ab9f76fe59293957670c358272ed736d28271794
Hamidul Islam <hamidthedev@gmail.com>
2024-01-08 21:22:01 +0600

    code updated

--- BinaryTree/Problems/NumOfNodes.py
+++ BinaryTree/Problems/NumOfNodes.py
@@ -12,7 +12,7 @@ node3 = TreeNode(3)
 root.left = node2
 root.right = node3
 
-#logic: only lastNode will return 0 (according to base case) as it's right and left is None then we add +1 to left and rightCount so every 
+#logic: only lastNode will return 0 (according to base case) as it's right and left is None then we add +1 to left and rightCount so every node will return 1
 def numOfNodes(root):
     if root is None:
         return 0
