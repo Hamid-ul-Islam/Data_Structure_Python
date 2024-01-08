5ef0da7a92a32793cd45efa62e471323db97f46e
Hamidul Islam <hamidthedev@gmail.com>
2024-01-08 21:50:50 +0600

    code updated

--- BinaryTree/Problems/LargestData.py
+++ BinaryTree/Problems/LargestData.py
@@ -13,7 +13,7 @@ node3 = TreeNode(3)
 rootNode.left = node2
 rootNode.right = node3
 
-#logic: last most node will 
+#logic: last most node will return -1 
 def largestData(root):
     if root is None:
         return -1
