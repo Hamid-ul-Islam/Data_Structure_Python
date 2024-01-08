92f76f4d45934b00b021e8cbb279163062571720
Hamidul Islam <hamidthedev@gmail.com>
2024-01-08 13:12:07 +0600

    code updated

--- BinaryTree/BinaryTree.py
+++ BinaryTree/BinaryTree.py
@@ -56,7 +56,7 @@ def treeInput():
   
   #creating a rootNode, also left and right node for the root node each time
   rootNode = TreeNode(data)
-  leftNode = TreeNode(treeInput()) #treeInput() again called and it will create leftNode() ask for left and right for it
+  leftNode = TreeNode(treeInput()) #treeInput() again called and it will create leftNode(root) ask for left and right for it
   rightNode = TreeNode(treeInput())
   
   #linking created node with each
