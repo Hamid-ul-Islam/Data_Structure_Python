ef80aee650338e356ff807671378cfc2cf0008c3
Hamidul Islam <hamidthedev@gmail.com>
2024-01-08 14:12:02 +0600

    code updated

--- BinaryTree/BinaryTree.py
+++ BinaryTree/BinaryTree.py
@@ -46,5 +46,5 @@ printTreeDetailed(treeInput())
 
 #taking input with list comprehension [5 3 -1 -1 4 -1 -1]
 def treeInputUseful():
-    num_elements = int(input("Enter the number of Nodes: "))
+    num_nodes = int(input("Enter the number of Nodes: "))
     my_list = [int(input("Enter data: ")) for i in range(num_elements)]
