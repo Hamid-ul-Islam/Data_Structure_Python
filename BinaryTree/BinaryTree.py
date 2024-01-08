d81c422946c0bcd5a1be28f9c25c24c8e087e85f
Hamidul Islam <hamidthedev@gmail.com>
2024-01-08 13:43:20 +0600

    code updated

--- BinaryTree/BinaryTree.py
+++ BinaryTree/BinaryTree.py
@@ -1,5 +1,5 @@
 #importing printing and taking input functions
-from PrintTree import printTree, printTreeDetailed
+from PrintTree import printTree
 from InputBinaryTree import treeInput
 
 #binary tree node class
