66a62adcdb7c33c6fca996de595c2c991664dfb9
Hamidul Islam <hamidthedev@gmail.com>
2024-01-08 22:25:51 +0600

    code updated

--- BinaryTree/Problems/4. NumOfLeafNodes.py
+++ BinaryTree/Problems/4. NumOfLeafNodes.py
@@ -18,7 +18,7 @@ node2.left = node4
 node2.right = node5
 
 def numOfLeafNodes(root):
-    if root is None:
+    if root. is None:
         return 1
     leafNodeOnLeft = numOfLeafNodes(root.left)
     leafNodeOnRight = numOfLeafNodes(root.right)
