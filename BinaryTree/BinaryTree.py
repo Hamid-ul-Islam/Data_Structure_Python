7353889f41d33c870bda026bf2b78a0907ad854d
Hamidul Islam <hamidthedev@gmail.com>
2024-01-08 14:27:23 +0600

    code updated

--- BinaryTree/BinaryTree.py
+++ BinaryTree/BinaryTree.py
@@ -55,7 +55,7 @@ def treeInputUseful(n):
     if data == -1:
         return None
     root = TreeNode(data)
-    leftNode = treeInputUseful(n+)
+    leftNode = treeInputUseful()
     rightNode = treeInputUseful()
     
     root.left = leftNode
