ba721a41f7b5ec33f9d46586cc408d46d2cf2b88
Hamidul Islam <hamidthedev@gmail.com>
2024-01-08 14:04:47 +0600

    code updated

--- BinaryTree/PrintTree.py
+++ BinaryTree/PrintTree.py
@@ -16,7 +16,7 @@ def printTreeDetailed(root):
     return
   if root.right is not None:
     print(" R->", root.right.data)
-  print(root.data) #root node data
+  print(root.data, "|") #root node data
   if root.left is not None:
     print(" L->",root.left.data)
   print("") #for new line after one node (with left and right node) is printed
