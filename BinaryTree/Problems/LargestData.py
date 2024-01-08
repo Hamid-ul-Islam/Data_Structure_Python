abccbf3f8861ee2f13122e543b31bdcb82a1a1cd
Hamidul Islam <hamidthedev@gmail.com>
2024-01-08 21:43:48 +0600

    code updated

---
+++ BinaryTree/Problems/LargestData.py
@@ -0,0 +1,14 @@
+#creating treenode
+class TreeNode:
+   def __init__(self, data):
+       self.data = data
+       self.left = None
+       self.right = None
+        
+
+rootNode = TreeNode(10)
+node2 = TreeNode(5)
+node3 = TreeNode(3)
+
+rootNode.left = node2
+rootNode.right = node3
