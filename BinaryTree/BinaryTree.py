3152009a5cec49d2231689f94a1dcde19cd0ac5c
Hamidul Islam <hamidthedev@gmail.com>
2024-01-08 12:17:51 +0600

    code updated

---
+++ BinaryTree/BinaryTree.py
@@ -0,0 +1,15 @@
+#binary tree node class
+class TreeNode:
+ def __init__(self, data):
+  self.data = data
+  self.left = None
+  self.right = None
+
+#creating node with node class
+rootNode = TreeNode(5)
+node2 = TreeNode(10)
+node3 = TreeNode(20)
+
+#linking node with each other
+rootNode.left = node2
+rootNode.right = node3
