9c29833d62a5c0e7127a085be2a2e486975b4ad4
Hamidul Islam <hamidthedev@gmail.com>
2024-01-09 23:17:12 +0600

    code updated

--- GenericTree/GenericTree.py
+++ GenericTree/GenericTree.py
@@ -1,4 +1,4 @@
-#Generic tree can have many children for a single node so the node will contains 
+#Generic tree can have many children for a single node so the node will contains data and a list of the 
 class Node:
     def __init__(self, data) -> None:#here None is the return type of the function and it's optional in python
         self.data = data
