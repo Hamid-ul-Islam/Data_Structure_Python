994a364cfabdfccb35a479f7ed25f78a420d6c8a
Hamidul Islam <hamidthedev@gmail.com>
2024-01-09 23:16:09 +0600

    code updated

--- GenericTree/GenericTree.py
+++ GenericTree/GenericTree.py
@@ -1,4 +1,4 @@
-class TreeNode:
+class Node:
     def __init__(self, data) -> None:#here None is the return type of the function and it's optional in python
         self.data = data
         self.children = []
