f88f5823c571dcb6e7ebe365b9a8935dfbe3da3b
Hamidul Islam <hamidthedev@gmail.com>
2024-01-09 23:33:08 +0600

    code updated

--- GenericTree/GenericTree.py
+++ GenericTree/GenericTree.py
@@ -25,7 +25,7 @@ n2.children.append(n7)
 
 #printing generic trees
 def printTree(root, index=0):
-    if root is not None:
+    if root is not None and :
         print(root.data)
     else:
         return
