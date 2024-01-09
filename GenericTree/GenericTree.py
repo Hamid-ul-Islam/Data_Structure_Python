594757bdb79c5879344c358b6e13629c96957e7a
Hamidul Islam <hamidthedev@gmail.com>
2024-01-09 23:50:15 +0600

    code updated

--- GenericTree/GenericTree.py
+++ GenericTree/GenericTree.py
@@ -40,4 +40,4 @@ printTree(n1) #giving root n1 as input
 #take generic tree input (for every input func we have to take input first and create a node. then we have to take care of the base case and then have to link nodes with each other)
 def takeInput():
     data = int(input())
-    
+    rootNode = 
