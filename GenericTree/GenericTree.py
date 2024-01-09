444497e9aa4a03f441e4c76ab8f130ea94ac0e7f
Hamidul Islam <hamidthedev@gmail.com>
2024-01-10 00:18:22 +0600

    code updated

--- GenericTree/GenericTree.py
+++ GenericTree/GenericTree.py
@@ -44,7 +44,7 @@ def takeInput():
         return None
 
     rootNode = Node(data)
-    while True: #
+    while True: #ta
         childNode = takeInput()
         if childNode is None: #children will None(returned from base case) when input value is -1 
             break
