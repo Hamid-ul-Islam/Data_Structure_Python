643299ae30adf7e99d710f393e43d8c4daa9154c
Hamidul Islam <hamidthedev@gmail.com>
2024-01-10 13:19:31 +0600

    code updated

--- GenericTree/NumberOfNodes.py
+++ GenericTree/NumberOfNodes.py
@@ -1,6 +1,6 @@
 #Generic tree can have many children for a single node so the node will contains data and a list of the child addresses
 class Node:
-    def __init__(self, data) -> None:#here None is the return type of the function and it's optional in python
+    def __init__(self, data):#here None is the return type of the function and it's optional in python
         self.data = data
         self.children = [] #addresses of the child nodes
 
