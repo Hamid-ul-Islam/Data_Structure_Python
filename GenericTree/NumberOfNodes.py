2e581d53a4e9df69ab53897deddecb914a57cfa9
Hamidul Islam <hamidthedev@gmail.com>
2024-01-10 13:23:09 +0600

    code updated

--- GenericTree/NumberOfNodes.py
+++ GenericTree/NumberOfNodes.py
@@ -23,4 +23,4 @@ n2.children.append(n6)
 n2.children.append(n7)
 
 
-#finding the number of nodes logic: every leaf nodes
+#finding the number of nodes logic: every leaf nodes has no child and it should return 
