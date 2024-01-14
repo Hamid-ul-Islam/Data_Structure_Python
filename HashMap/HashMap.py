364b4d5cb7627a4c75af203136faedfd8eb5ec81
Hamidul Islam <hamidthedev@gmail.com>
2024-01-14 22:16:07 +0600

    code updated

--- HashMap/HashMap.py
+++ HashMap/HashMap.py
@@ -1,4 +1,4 @@
-#hashmap node : simply a list where every block contains a LinkedList. when we try to insert a key value pair into the list we module  HashCod(key) % size of the buckets/list and we find a specific index. we try to insert into the index. if any other keys HashCod(key) % size
+#hashmap node : simply a list where every block contains a LinkedList. when we try to insert a key value pair into the list we module  HashCod(key) % size of the buckets/list and we find a specific index. we try to insert into the index. if any other keys (HashCod(key) % size) 
 class MapNode:
     def __init__(self, key, value):
         self.key = key
