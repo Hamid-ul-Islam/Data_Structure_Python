b35bf1924222f9e750eca74d86bd0bc2476754c5
Hamidul Islam <hamidthedev@gmail.com>
2024-01-15 00:28:17 +0600

    code updated

--- Graph/Graph.py
+++ Graph/Graph.py
@@ -3,7 +3,7 @@ class Graph:
     def __init__(self, vertices):
         self.vertices = vertices
         self.adjMatrix = [[0 for i in range(vertices)] for j in range(vertices)] #short form of listcomprehension [[0 for i in range(vertices)] for j in range(vertices)]
-        
+        print(adj)
     def addEdge(self, v1, v2):
         self.adjMatrix[v1][v2] = 1
         self.adjMatrix[v2][v2] = 1
