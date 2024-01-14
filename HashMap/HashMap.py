8f0b2cd665599ea7ffc00497449eadffdc4b66e8
Hamidul Islam <hamidthedev@gmail.com>
2024-01-14 20:30:22 +0600

    code updated

---
+++ HashMap/HashMap.py
@@ -0,0 +1,36 @@
+#hashmap
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+
+class HashMap:
+    def __init__(self, size):
+        self.size = size
+        self.buckets = [None] * size
+
+    def hash(self, key):
+        return hash(key) % self.size
+
+    def insert(self, key, value):
+        index = self.hash(key)
+        if not self.buckets[index]:
+            self.buckets[index] = Node(key, value)
+        else:
+            current = self.buckets[index]
+            while current.next:
+                if current.key == key:
+                    current.value = value
+                    return
+                current = current.next
+            current.next = Node(key, value)
+
+    def get(self, key):
+        idx = self.hash(key)
+        current = self.buckets[idx]
+        while current:
+            if current.key == key:
+                return current.value
+            current = current.next
+        return None
