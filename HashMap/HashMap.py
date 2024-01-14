06b75c30b60c88068adef05c56b938790ac18a23
Hamidul Islam <hamidthedev@gmail.com>
2024-01-14 23:28:46 +0600

    code updated

--- HashMap/HashMap.py
+++ HashMap/HashMap.py
@@ -37,7 +37,7 @@ class HashMap: #a list containing a linkedlist in each blocks (initialiezed with
         while head:
             if head.key == key:
                 if prev is None:
-                    self.buckets[index] = None
+                    self.buckets[index] = None #prev is None for the first node and 
                     self.count -= 1
                 else:
                     prev.next = head.next
