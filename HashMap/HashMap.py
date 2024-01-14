471e98d2ed03be6a9e88ee5a9348816819f7b481
Hamidul Islam <hamidthedev@gmail.com>
2024-01-14 22:21:10 +0600

    code updated

--- HashMap/HashMap.py
+++ HashMap/HashMap.py
@@ -12,7 +12,7 @@ class HashMap: #a list containing a linkedlist in each blocks (initialiezed with
         self.bucketSize = size
         self.count = 0
 
-    def hash(self, key): # this returns index within the bucketsize (sometime)
+    def hash(self, key): # this returns index within the bucketsize ()
         return (abs(hash(key)) % self.bucketSize)
 
     def insert(self, key, value):
