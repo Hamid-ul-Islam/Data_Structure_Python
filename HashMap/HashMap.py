f74e0a6c0bbfbbeadfa5ad7a90276269ca453515
Hamidul Islam <hamidthedev@gmail.com>
2024-01-14 23:30:25 +0600

    code updated

--- HashMap/HashMap.py
+++ HashMap/HashMap.py
@@ -43,7 +43,7 @@ class HashMap: #a list containing a linkedlist in each blocks (initialiezed with
                     prev.next = head.next
                     self.count -= 1
             
-            #until head is not not None and head.key != key while loop will continue by prev=head and head = head.next
+            #until head is not not None and head.key != key while loop will continue by prev will move to head and head will move 
             prev = head 
             head = head.next
         return None
