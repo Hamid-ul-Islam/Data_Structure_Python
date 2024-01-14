8af7e711f678edc7d7c217327e3fd01a8ca18001
Hamidul Islam <hamidthedev@gmail.com>
2024-01-14 23:49:23 +0600

    code updated

--- HashMap/HashMap.py
+++ HashMap/HashMap.py
@@ -66,6 +66,7 @@ class HashMap: #a list containing a linkedlist in each blocks (initialiezed with
 #creating map of size 10 initialized with None
 map = HashMap(10)
 map.insert("ab", 60)
+map.insert("abaa")
 
 #inserting
 map.insert("Hamid", 19)
