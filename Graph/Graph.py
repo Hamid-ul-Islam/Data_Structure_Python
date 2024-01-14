e9a1ff3522f6429debdaed65042dd2b56baec71e
Hamidul Islam <hamidthedev@gmail.com>
2024-01-15 00:29:13 +0600

    code updated

--- Graph/Graph.py
+++ Graph/Graph.py
@@ -6,7 +6,7 @@ class Graph:
         print(self.adjMatrix)
     def addEdge(self, v1, v2):
         self.adjMatrix[v1][v2] = 1
-        self.adjMatrix[v2][v2] = 1
+        self.adjMatrix[v2][v1] = 1
 
     def removeEdge(self, v1, v2):
         if self.hasEdge(v1, v2) is False:
