c8e070dc93ce6b06620f95ddb3bdc65bc05fe240
Hamidul Islam <hamidthedev@gmail.com>
2024-01-15 00:15:17 +0600

    code updated

--- Graph/Graph.py
+++ Graph/Graph.py
@@ -1,5 +1,5 @@
-#graph implementation with adjMatrix  (initialized all matrix with 0)
+#graph implementation with adjMatrix (2d array) (initialized all matrix with 0)
 class Graph:
     def __init__(self, vertices):
         self.vertices = vertices
-        self.adjMatrix
+        self.adjMatrix = 
