796357cadd2fa88cbfa16f52a3105ead7cd1c83c
Hamidul Islam <hamidthedev@gmail.com>
2024-01-15 00:18:06 +0600

    code updated

--- Graph/Graph.py
+++ Graph/Graph.py
@@ -5,5 +5,6 @@ class Graph:
         self.adjMatrix = [[0] * vertices] * vertices #short form of listcomprehension [[0 for i in range(vertices)] for j in range(vertices)]
         
     def addEdge(self):
+        
     def removeEdge(self):
     def hasEdge(self)
