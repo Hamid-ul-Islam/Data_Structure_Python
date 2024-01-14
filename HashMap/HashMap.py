f0d2139f8d3859abe3b33980ca6010bbd100bca2
Hamidul Islam <hamidthedev@gmail.com>
2024-01-14 21:31:51 +0600

    code updated

--- HashMap/HashMap.py
+++ HashMap/HashMap.py
@@ -15,7 +15,7 @@ class HashMap:
         return (abs(hash(key)) % self.size)
 
     def insert(self, key, value):
-        index = hash(key)
+        index = self.hash(key)
         
 
     def get(self, key):
