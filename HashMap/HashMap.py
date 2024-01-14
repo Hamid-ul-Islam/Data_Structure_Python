72383599cb53fcdec25c2cd4cf96bfbfb0d237da
Hamidul Islam <hamidthedev@gmail.com>
2024-01-14 22:24:47 +0600

    code updated

--- HashMap/HashMap.py
+++ HashMap/HashMap.py
@@ -17,7 +17,7 @@ class HashMap: #a list containing a linkedlist in each blocks (initialiezed with
 
     def insert(self, key, value):
         index = self.hash(key)
-        head = self.buckets[index] #first find the head of 
+        head = self.buckets[index] #first find the head of first entry of the list and try to 
         while head: #while head is not None
             if head.key == key:
                 head.value = value
