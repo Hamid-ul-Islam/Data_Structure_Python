e951fa33273cb757e21d57d1ace83eb3b0c6f20a
Hamidul Islam <hamidthedev@gmail.com>
2024-01-18 13:37:59 +0600

    code updated

---
+++ DP/FinacciDP.py
@@ -0,0 +1,13 @@
+def fibonacci(n):
+    fib = [0] * (n + 1)  # Initialize an array to store Fibonacci numbers
+    fib[1] = 1           # Set the base case F(1) = 1
+
+    for i in range(2, n + 1):
+        fib[i] = fib[i - 1] + fib[i - 2]  # Use the recurrence relation to calculate Fibonacci numbers
+
+    return fib[n]
+
+# Example usage
+n = 10
+result = fibonacci(n)
+print(f"The {n}th Fibonacci number is: {result}")
