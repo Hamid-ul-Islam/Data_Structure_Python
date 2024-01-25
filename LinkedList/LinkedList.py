3b0e9176d0a5f39d3d1a0d7d1602ae8aba4fd8f8
Hamidul Islam <hamidthedev@gmail.com>
2024-01-25 10:44:56 +0600

    code updated

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -39,5 +39,5 @@ def takeInput():
             head = newNode
             tail = newNode
         else:
-            tail.next = newNode #as head and tail was previously same, so tail.next means head.next
+            tail.next = newNode #as head and tail was previously same, so tail.next means head.next. by doing tail.next
             tail = newNode
